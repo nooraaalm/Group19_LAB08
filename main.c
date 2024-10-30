26ed3185831f6d125494b3970cd0fe822dfa27c7
Noor Alam <nooralam.shine2017@gmail.com>
2024-10-31 00:38:07 +0530

    adding header file

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
 
 
-/**
- * main.c
- */
-int main(void)
-{
-	return 0;
-}
+void UART0_Init(void);
+char UART0_Received(void);
+void UART0_Send(char data);
+void LED_Init(void);
+void LED_ON_OFF(char choice);
+
