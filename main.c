ab99103bd8a3f9392a6e4e46dddeba25afbb0652
Noor Alam <nooralam.shine2017@gmail.com>
2024-10-30 23:41:41 +0530

    calling functions in the main functions

--- main.c
+++ main.c
@@ -7,4 +7,15 @@ char UART0_Received(void);
 void UART0_Send(char data);
 void LED_Init(void);
 void LED_ON_OFF(char choice);
+nt main(void) {
 
+    UART0_Init();
+
+    char receivedChar;
+    while (1) {
+
+        receivedChar = UART0_Received();
+        UART0_Send(receivedChar);
+        LED_ON_OFF(receivedChar);
+    }
+}
