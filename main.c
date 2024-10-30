c682e13f4313644e44005931d46e652e0782b4e9
avulanitheesh <ee24mt032@iitdh.ac.in>
2024-10-30 23:33:38 +0530

    created uart0 and led required functions

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+
+void UART0_Init(void);
+char UART0_Received(void);
+void UART0_Send(char data);
+void LED_Init(void);
+void LED_ON_OFF(char choice);
 
